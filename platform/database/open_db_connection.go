d0edf3bd84ae13895c3b7068e23c8ae87d84281d
akshayshcoder <akshayshcoder@gmail.com>
2024-09-09 22:53:47 +0530

    fiber commit

---
+++ platform/database/open_db_connection.go
@@ -0,0 +1,24 @@
+// ./platform/database/open_db_connection.go
+
+package database
+
+import "github.com/koddr/tutorial-go-fiber-rest-api/app/queries"
+
+// Queries struct for collect all app queries.
+type Queries struct {
+	*queries.BookQueries // load queries from Book model
+}
+
+// OpenDBConnection func for opening database connection.
+func OpenDBConnection() (*Queries, error) {
+	// Define a new PostgreSQL connection.
+	db, err := PostgreSQLConnection()
+	if err != nil {
+		return nil, err
+	}
+
+	return &Queries{
+		// Set queries from models:
+		BookQueries: &queries.BookQueries{DB: db}, // from Book model
+	}, nil
+}
